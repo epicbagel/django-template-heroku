5fdd17a634b29a398ab94a61759668da4eaafc23
Jon Bolt <epic.bagel@gmail.com>
2013-12-03 23:25:22 +0000

    Update heroku.py

--- project_name/settings/heroku.py
+++ project_name/settings/heroku.py
@@ -1,12 +1,32 @@
 from base import *
+import os
+
+# Set up redis
+from redisify import redisify
+CACHES = redisify(default='redis://localhost')
 
 # Parse database configuration from $DATABASE_URL
-import dj_database_url
-DATABASES['default'] = dj_database_url.config()
+from postgresify import postgresify
+DATABASES = postgresify()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Allow all host headers
 ALLOWED_HOSTS = ['*']
+
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = 'labelstate'
+AWS_QUERYSTRING_AUTH = False
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+MEDIA_URL = "https://{0}.s3.amazonaws.com/media/".format(AWS_STORAGE_BUCKET_NAME)
+
+#STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+STATICFILES_STORAGE = 'storagescache.boto.CachedStaticS3BotoStorage'
+STATIC_URL = "https://{0}.s3.amazonaws.com/static/".format(AWS_STORAGE_BUCKET_NAME)
+
+
+
 
